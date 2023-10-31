5a153f167b322ac53c890c972ea5de34b505311b
Gianni Ioannou <108948908+gi6723@users.noreply.github.com>
2023-10-31 12:38:17 -0500

    orgainziation

--- StockEngine.py
+++ StockEngine.py
@@ -3,7 +3,6 @@ import os
 import requests
 from dotenv import load_dotenv
 
-
 driver = webdriver.Chrome()
 load_dotenv("/Users/gianniioannou/Documents/GitHub Files/Real-time-Stock-Trend-Prediction-Engine/ApiKey.env")
 api_key = os.getenv('TWELVE_DATA_API_KEY')
