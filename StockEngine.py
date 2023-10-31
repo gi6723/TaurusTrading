6c13d749383ac9e7ef67cd1304a1922bca6557f1
Gianni Ioannou <108948908+gi6723@users.noreply.github.com>
2023-10-30 22:13:46 -0500

    Put back in .env file

--- StockEngine.py
+++ StockEngine.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
-from time import sleep
 import os
 import requests
-from datetime import datetime, timedelta
 from dotenv import load_dotenv
-from pytz import timezone
+
 
 driver = webdriver.Chrome()
 load_dotenv("/Users/gianniioannou/Documents/GitHub Files/Real-time-Stock-Trend-Prediction-Engine/ApiKey.env")
