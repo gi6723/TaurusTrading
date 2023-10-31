a72ef62d58fb5c8c9eb109b39f6b649171d3d31d
Gianni Ioannou <108948908+gi6723@users.noreply.github.com>
2023-10-31 13:03:27 -0500

    Structured the Project to start

--- src/api/stock_api.py
+++ src/api/stock_api.py
@@ -1,9 +1,5 @@
-from selenium import webdriver
 import os
-import requests
 from dotenv import load_dotenv
 
-driver = webdriver.Chrome()
 load_dotenv("/Users/gianniioannou/Documents/GitHub Files/Real-time-Stock-Trend-Prediction-Engine/ApiKey.env")
 api_key = os.getenv('TWELVE_DATA_API_KEY')
-
