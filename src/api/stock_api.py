699795a38f499a445803d4a9fa66373d5f18cc74
Gianni Ioannou <108948908+gi6723@users.noreply.github.com>
2023-12-19 00:36:55 -0600

    Starting From Scratch

--- src/api/stock_api.py
+++
@@ -1,5 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv("/Users/gianniioannou/Documents/GitHub Files/Real-time-Stock-Trend-Prediction-Engine/ApiKey.env")
-api_key = os.getenv('TWELVE_DATA_API_KEY')
