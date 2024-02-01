fcd4b046a930ead0522deb62d7515cd9a8edb8b4
Gianni Ioannou <108948908+gi6723@users.noreply.github.com>
2024-01-31 23:52:24 -0600

    database setup start

--- backend/preMarketDataCollection.py
+++ backend/preMarketDataCollection.py
@@ -27,11 +27,12 @@ class DataCollection:
         self.add_input_for_login(by=By.NAME, value="password", text = password)
         self.uncheck_remember_me_for_login()
         self.submit_for_login()
-    
+    #Helper Function for login, takes in the values I pass in and inputs them into the login fields
     def add_input_for_login(self, by: By, value: str, text:str):
         field = self.driver.find_element(by=by, value=value)
         field.send_keys(text)
-        
+
+    #Helper Function for login, clicks the login button    
     def submit_for_login(self):
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.CLASS_NAME, "fv-button"))
@@ -39,7 +40,7 @@ class DataCollection:
         button = self.driver.find_element(by=By.CLASS_NAME, value="fv-button")
         button.click()
 
-        
+    #Helper Function for login, unchecks the remember me checkbox
     def uncheck_remember_me_for_login(self):
         checkbox = self.driver.find_element(by=By.NAME, value="remember")
         self.driver.execute_script("arguments[0].scrollIntoView(true);", checkbox)
@@ -50,7 +51,8 @@ class DataCollection:
             except ElementClickInterceptedException:
                 # If standard click fails, use JavaScript click
                 self.driver.execute_script("arguments[0].click();", checkbox)
-
+    
+    #Navigates to the screener tab and selects the "s: PreJump" preset
     def navigate_to_screener(self):
         screener_tab = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[3]/a"))
@@ -63,7 +65,8 @@ class DataCollection:
         preset_object = Select(preset_element)
         preset_object.select_by_visible_text("s: PreJump")
 
-        
+    def grab_info_from_table(self):
+        print("Ill figure it out trust")
     
     def get_data(self):
         self.login()
