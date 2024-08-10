5eeb80dfd05eaed54e1f0295fb926c67df4c272e
Gianni Ioannou <108948908+gi6723@users.noreply.github.com>
2024-08-09 22:01:17 -0500

    lots of work with Trading View
    Was able to get to the point where I can now check for relevant times. Now all thats left is to grab the Hrefs and to enter into a new tab then grab article data and text

--- backend/web_scrapers/tradingview_article_scraper.py
+++ backend/web_scrapers/tradingview_article_scraper.py
@@ -1,5 +1,7 @@
 import json
 import os
+from datetime import datetime
+import pytz
 import time
 from dotenv import load_dotenv
 from selenium import webdriver
@@ -8,6 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from fake_useragent import UserAgent
 import requests
 import whisper
@@ -23,6 +26,13 @@ class TradingViewArticleScraper:
         self.driver = webdriver.Firefox(options=options)
         self.user_agent = UserAgent()
         os.environ['SSL_CERT_FILE'] = certifi.where()
+
+    def grab_tickers(self):
+        with open("/Users/gianniioannou/Documents/GitHub Files/TaurusTrading/backend/temp.json", "r") as f:
+            data = json.load(f)
+            tickers = [item["Ticker"] for item in data]
+        f.close()
+        return tickers
 
     def add_input_for_login(self, by: By, value: str, text: str):
         try:
@@ -109,16 +119,30 @@ class TradingViewArticleScraper:
             WebDriverWait(self.driver, 10).until(
                 EC.frame_to_be_available_and_switch_to_it((By.XPATH, ".//iframe[@title='recaptcha challenge expires in two minutes']"))
             )
-            audio_url = self.driver.find_element(By.ID, "audio-source").get_attribute("src")
-            transcription = self.transcribe(audio_url)
-            if transcription:
-                audio_response_field = self.driver.find_element(By.ID, "audio-response")
-                audio_response_field.send_keys(transcription)
-                verify_button = self.driver.find_element(By.ID, "recaptcha-verify-button")
-                self.scroll_and_click(verify_button)
-                print("Audio challenge solved")
-            else:
-                print("Failed to transcribe audio")
+            while True:
+                audio_url = self.driver.find_element(By.ID, "audio-source").get_attribute("src")
+                transcription = self.transcribe(audio_url)
+
+                if transcription:
+                    audio_response_field = self.driver.find_element(By.ID, "audio-response")
+                    audio_response_field.send_keys(transcription)
+                    verify_button = self.driver.find_element(By.ID, "recaptcha-verify-button")
+                    self.scroll_and_click(verify_button)
+                    print("submitted transcription")
+
+                    time.sleep(2)
+
+                    try:
+                        error_message = self.driver.find_element(By.CLASS_NAME, "rc-audiochallenge-error-message")
+                        if error_message.is_displayed():
+                            print("Error message detected: Multiple correct solutions required. Retrying...")
+                            continue
+                    except NoSuchElementException:
+                        print("No error message detected, moving on")
+                        break    
+
+                else:
+                    print("Failed to transcribe audio")
         except Exception as e:
             print(f"An error occurred while solving the audio challenge: {e}")
         finally:
@@ -176,24 +200,88 @@ class TradingViewArticleScraper:
                 print(f"No Recaptcha appeared or error occurred: {e}")
         except Exception as e:
             print(f"An error occurred during login: {e}")
-
-    def filter_articles(self, articles, retries=3):
-        return
     
-    def fetch_articles(self, ticker):
-        return
+    def ticker_search(self, ticker):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "tv-header-search-container"))
+            ).click()
 
+            search_box = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "input-KLRTYDjH"))
+            )
+            search_box.clear()
+            search_box.send_keys(ticker)
+            search_box.send_keys(Keys.RETURN)
+
+            self.switch_to_news_tab()
+
+        except TimeoutException:
+            print("Search box not found or not clickable")
+        except NoSuchElementException:
+            print("Search bar element could not be found")
+        except Exception as e:
+            print(f"An unexpected error occurred in ticker_search: {e}")
+
+    def switch_to_news_tab(self):
+        try:
+            news_tab = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//a[contains(@class, 'js-category-tab') and contains(@href, '/news/')]"))
+            )
+            news_tab.click()
+    
+        except TimeoutException:
+            print("News tab is not clickable or could not be found")
+       
+    def fetch_articles_and_text(self, tickers):
+        for ticker in tickers:
+            print(f"Searching for ticker: {ticker}")
+            self.ticker_search(ticker)
+            print(f"Finished searching for ticker: {ticker}, now looking for articles")
+            try:
+                container = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "list-iTt_Zp4a"))
+                )
+                print("Found article container")
+
+                self.driver.execute_script("arguments[0].scrollIntoView(true);", container)
+                time.sleep(2)
+
+                for row in container.find_elements(By.XPATH, ".//a[contains(@class, 'card-HY0D0owe')]"):
+                    try:
+                        time_element = row.find_element(By.CLASS_NAME, "apply-common-tooltip")
+                        time_title = time_element.get_attribute("title")
+
+                        time_posted_naive = datetime.strptime(time_title, '%b %d, %Y, %H:%M %Z')
+                        tz = pytz.timezone('US/Central') 
+                        time_posted = tz.localize(time_posted_naive)
+
+                        now = datetime.now(tz)
+                        time_diff = now - time_posted
+                        time_diff_minutes = time_diff.total_seconds() / 60
+                        print(f"Time difference: {time_diff_minutes} minutes ago")
+
+                        if time_diff_minutes > 24 * 60:
+                            break
+                    except Exception as e:
+                        print(f"Could not find time element in shadow DOM: {e}")
+            except Exception as e:
+                print(f"Could not find container for {ticker}: {e}")
+
+
+        
     def close(self):
         self.driver.quit()
 
-    def process_ticker(self, ticker):
+    def process_ticker(self):
         self.login()
-        self.fetch_articles(ticker)
+        tickers = self.grab_tickers()
+        self.fetch_articles_and_text(tickers)
         #self.driver.quit()
 
 if __name__ == "__main__":
     scraper = TradingViewArticleScraper()
-    scraper.process_ticker("NVDA")
+    scraper.process_ticker()
 
 
 
